29aca312b082adfcc9db01365b15ecf2c6c7447c
Mr.An <fuckyou@dick.com>
2017-05-09 07:39:48 +0100

    add final results

--- parallel_programming/course_work/test.sh
+++ parallel_programming/course_work/test.sh
@@ -3,6 +3,6 @@ do
 	for j in `seq 40 -10 10`;
 	do
 		echo $i $j
-		./main $((i*1200)) $j >> results.txt
+		./main $((i*960)) $j >> results.txt
 	done;
 done;
--- parallel_programming/course_work/test2.sh
+++ parallel_programming/course_work/test2.sh
@@ -3,6 +3,6 @@ do
 	for j in `seq 40 -10 10`;
 	do
 		echo $i $j
-		sh exec.sh $((i*1200)) $j >> results2.txt
+		sh exec.sh $((i*960)) $j >> results2.txt
 	done;
 done;
